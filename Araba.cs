4ac632a55574a8f2bbadaf09b6699c4827801d3d
Rukiye Ozturk <33896049+rguloztrk@users.noreply.github.com>
2019-01-03 19:56:04 +0300

    Add files via upload

---
+++ Araba.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabaGaleri
+{
+   
+    public abstract class ArabaTemplate //Model sinifindaki turlere uygulandi
+    {
+        protected virtual bool ModifiyeOlmusMu()
+        {
+            return false;
+        }
+
+        protected void ArabaKalibiCikar()
+        {
+            Console.WriteLine("- Arabanin model kalibi olusturuluyor\n");
+        }
+
+        protected abstract void MotorUretimi();//arabanin tipine gore motor gucu degiskenlik gosterir 4x4 8x8 beygir gucu vs.
+
+        protected void BoyamaIslemi()
+        {
+            Console.WriteLine("- Araba boyama isleminden geciyor\n");
+        }
+
+        protected abstract void TekerUretimi();//arabanin tipine gore teker boyutu surtunme seviyesi vs
+
+        public void ArabaUret()
+        {
+            ArabaKalibiCikar();
+            MotorUretimi();
+            BoyamaIslemi();
+            TekerUretimi();
+        }
+
+
+    }
+
+
+}
