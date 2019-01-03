7dfe6a272e35de917f882eee96084d192a9169c5
Rukiye Ozturk <33896049+rguloztrk@users.noreply.github.com>
2019-01-03 19:49:08 +0300

    Add files via upload

---
+++ Araba.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabaGaleri
+{
+    public class Araba
+    {
+        public int yil;
+        public Marka markasi;
+        public ModelDecorator modeli;
+        public Fiyat fiyati;
+        public OdemeContext odeme;
+
+    }
+}
