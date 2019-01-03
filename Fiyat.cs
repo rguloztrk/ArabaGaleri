1a8f0022df6d629b38d10091c1f6bee5078f0a6a
Rukiye Ozturk <33896049+rguloztrk@users.noreply.github.com>
2019-01-03 19:49:31 +0300

    Add files via upload

---
+++ Fiyat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabaGaleri
+{
+    public class Fiyat
+    {
+        public int Yil;
+        public float Butce;
+        public ArrayList ArabaFiyat;
+        public ArrayList ModelFiyat;
+
+        public Fiyat(int yil, float  butce)
+        {
+            this.Yil = yil;
+            this.Butce = butce;
+        }
+    }
+}
